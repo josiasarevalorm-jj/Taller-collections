2a2c39176ed6dda1db393878be2dc49a9cac4b04
unknown <josiasarevalo.rm@gmail.com>
2026-05-25 22:28:43 -0500

    hasshet

--- src/Test/testPelicula.java
+++ src/Test/testPelicula.java
@@ -3,7 +3,9 @@ package Test;
 import Clases.Pelicula;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class testPelicula {
 
@@ -11,6 +13,11 @@ public class testPelicula {
 
         List<Pelicula> listaPeliculas =
                 new ArrayList<Pelicula>();
+
+        Set<Pelicula> setPeliculas =
+                new HashSet<Pelicula>();
+
+        // AGREGAR PELICULAS
 
         listaPeliculas.add(
                 new Pelicula(
@@ -45,11 +52,37 @@ public class testPelicula {
                 )
         );
 
+        // MOSTRAR LISTA
+
         System.out.println(
                 "LISTA DE PELICULAS"
         );
 
         for(Pelicula pelicula : listaPeliculas){
+
+            System.out.println(pelicula);
+        }
+
+        //hashset
+
+        setPeliculas.addAll(listaPeliculas);
+
+        setPeliculas.add(
+                new Pelicula(
+                        "Batman",
+                        "Christopher Nolan",
+                        152,
+                        "Accion",
+                        2008,
+                        9.0
+                )
+        );
+
+        System.out.println(
+                "\nPELICULAS EN HASHSET"
+        );
+
+        for(Pelicula pelicula : setPeliculas){
 
             System.out.println(pelicula);
         }
